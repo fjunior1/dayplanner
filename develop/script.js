3774ecc61357632cd623db9b6968a5478d11f9ba
Franklyn Diaz <franklyn_diaz@hotmail.com>
2021-05-22 16:08:21 -0400

    Adding time areas, adding some css to time areas, adding function skeletons to JS(need to implement those functions)

---
+++ develop/script.js
@@ -0,0 +1,33 @@
+/*
+jQuery to get html element as var
+*/
+var saveButton = $('.btn'); // var to get save buttons
+var meetingNotes = $(".notes"); // meeting notes area var 
+
+/*
+get and set local storage
+*/
+function setStorage() {
+    
+}
+
+function getstorage() {
+    
+}
+
+/*
+process click on save button
+*/
+saveButton.on('click', function () {
+    // check which button was clicked and save those notes
+
+});
+
+/*
+process click on notes to edit
+*/
+meetingNotes.on('click', function () {
+    // check which area was clicked to edit
+
+});
+
